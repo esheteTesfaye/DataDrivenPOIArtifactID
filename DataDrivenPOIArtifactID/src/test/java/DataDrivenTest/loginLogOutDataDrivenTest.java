8dc7d0ffb90a7746933807d04846aa6bf7123039
eshete <eshete@DESKTOP-HCFFR5C>
2020-03-01 16:01:08 -0500

    added now cods

--- DataDrivenPOIArtifactID/src/test/java/DataDrivenTest/loginLogOutDataDrivenTest.java
+++ DataDrivenPOIArtifactID/src/test/java/DataDrivenTest/loginLogOutDataDrivenTest.java
@@ -16,42 +16,43 @@ import org.testng.annotations.DataProvider;
 
 public class loginLogOutDataDrivenTest {
 	WebDriver driver;
-	
-	@BeforeTest 
-	public void beforeTest(){
-	driver= new ChromeDriver();
-	driver.get("http://newtours.demoaut.com/");
+
+	@BeforeTest
+	public void beforeTest() {
+		driver = new ChromeDriver();
+		driver.get("http://newtours.demoaut.com/");
 	}
-	@AfterTest 
+
+	@AfterTest
 	public void afterTest() {
 		driver.quit();
 	}
-  @Test(dataProvider = "dp")
-  public void f(String n, String s) {
-	  
-	  driver.findElement(By.name("userName")).sendKeys(n);
-	  driver.findElement(By.name("password")).sendKeys(s);
-	  driver.findElement(By.name("login")).click();
-	  Assert.assertTrue(driver.findElement(By.linkText("SIGN-OFF")).isDisplayed());
-	  driver.findElement(By.partialLinkText("SIGN-OFF")).click();
-	  System.out.println(n + "....." + s);
-  }
 
-  @DataProvider
-  public Object[][] dp() throws IOException {
-	  // call a method to read from excel file
-	  // store the record to two dimensional array
-	  // return the array 
-	    ReadDataFromExcelFile readExcelObject = new ReadDataFromExcelFile();
-		 String filePath = "C:\\Users\\eshet\\eclipse-workspace\\DataDrivenPOIArtifactID\\src\\test\\java\\TestData\\Data.xlsx";
-		 String[][] data = readExcelObject.readEcel(filePath, "Sheet1");
-		 
-		 return data; 
-		 
-   /* return new Object[][] {
-      new Object[] { "1", "a" },
-      new Object[] { "2", "b" },
-      new Object[] { "c", "c" },
-    };*/
-  }
+	@Test(dataProvider = "dp")
+	public void f(String n, String s) {
+
+		driver.findElement(By.name("userName")).sendKeys(n);
+		driver.findElement(By.name("password")).sendKeys(s);
+		driver.findElement(By.name("login")).click();
+		Assert.assertTrue(driver.findElement(By.linkText("SIGN-OFF")).isDisplayed());
+		driver.findElement(By.partialLinkText("SIGN-OFF")).click();
+		System.out.println(n + "....." + s);
+	}
+
+	@DataProvider
+	public Object[][] dp() throws IOException {
+		// call a method to read from excel file
+		// store the record to two dimensional array
+		// return the array
+		ReadDataFromExcelFile readExcelObject = new ReadDataFromExcelFile();
+		String filePath = "C:\\Users\\eshet\\eclipse-workspace\\DataDrivenPOIArtifactID\\src\\test\\java\\TestData\\Data.xlsx";
+		String[][] data = readExcelObject.readEcel(filePath, "Sheet1");
+
+		return data;
+
+		/*
+		 * return new Object[][] { new Object[] { "1", "a" }, new Object[] { "2", "b" },
+		 * new Object[] { "c", "c" }, };
+		 */
+	}
 }
--- DataDrivenPOIArtifactID/src/test/java/ReadingFromFile/ReadDataFromExcelFile.java
+++ DataDrivenPOIArtifactID/src/test/java/ReadingFromFile/ReadDataFromExcelFile.java
@@ -11,39 +11,39 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadDataFromExcelFile {
 	public String[][] readEcel(String filePath, String sheetName) throws IOException {
-		// Create an object of FileInpute stream 
-		FileInputStream inputStream = new FileInputStream(filePath); 
-		// reading from excel file 
+		// Create an object of FileInpute stream
+		FileInputStream inputStream = new FileInputStream(filePath);
+		// reading from excel file
 		// use AppachePOI
-		Workbook workBook  = new XSSFWorkbook(inputStream); 
-			
-		
+		Workbook workBook = new XSSFWorkbook(inputStream);
+
 		Sheet workSheet = workBook.getSheet(sheetName);
 		int rowCount = workSheet.getLastRowNum() - workSheet.getFirstRowNum();
-		String[][] dataFromExcel = new String [rowCount][2]; 
-		for(int i = 1; i < rowCount; i++) {
-		Row row = workSheet.getRow(i);
-		dataFromExcel [i][0]=row.getCell(0).getStringCellValue();
-		dataFromExcel [i][1]=row.getCell(1).getStringCellValue();	
-		
-	}
-	return dataFromExcel;
+		String[][] dataFromExcel = new String[rowCount][2];
+		for (int i = 1; i < rowCount; i++) {
+			Row row = workSheet.getRow(i);
+			dataFromExcel[i][0] = row.getCell(0).getStringCellValue();
+			dataFromExcel[i][1] = row.getCell(1).getStringCellValue();
 
-}
-	public static void main (String[] args) throws IOException { 
+		}
+		return dataFromExcel;
+
+	}
+
+	public static void main(String[] args) throws IOException {
 		ReadDataFromExcelFile readExcelObject = new ReadDataFromExcelFile();
-		 String filePath = "C:\\Users\\eshet\\eclipse-workspace\\DataDrivenPOIArtifactID\\src\\test\\java\\TestData\\Data.xlsx";
-		 String[][] data = readExcelObject.readEcel(filePath, "Sheet1");
-		for(int j = 0; j < data.length; j++ ) {
+		// String filePath =
+		// "C:\\Users\\eshet\\eclipse-workspace\\DataDrivenPOIArtifactID\\src\\test\\java\\TestData\\Data.xlsx";
+		String filePath = "C:\\Users\\eshet\\git\\DataDriven\\DataDrivenPOIArtifactID\\src\\test\\java\\TestData\\Data.xlsx";
+		String[][] data = readExcelObject.readEcel(filePath, "Sheet1");
+		for (int j = 0; j < data.length; j++) {
 			System.out.println(data[j][0] + "....." + data[j][1]);
 			// opne the broeser
 			// open newtorus login paage
-			// type user name and password 
-			// press loing and assert 
-			
-			
+			// type user name and password
+			// press loing and assert
+
 		}
 	}
 
-	
 }
